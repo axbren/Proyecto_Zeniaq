8b9d99593478c6a5e9bfc1aaac40c3194b91ff16
axbren <natsuevans1999@gmail.com>
2021-03-02 16:25:07 -0600

    modificacion contabilidad electronica

---
+++ NextP/.next/static/webpack/pages/Contabilidad_electronica.7feded541d3b02cd3bd0.hot-update.js
@@ -0,0 +1,97 @@
+webpackHotUpdate_N_E("pages/Contabilidad_electronica",{
+
+/***/ "./pages/Contabilidad_electronica.js":
+/*!*******************************************!*\
+  !*** ./pages/Contabilidad_electronica.js ***!
+  \*******************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* WEBPACK VAR INJECTION */(function(module) {/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ "./node_modules/react/jsx-dev-runtime.js");
+/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_Container__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../components/Container */ "./components/Container.js");
+
+
+var _jsxFileName = "C:\\Users\\ADMIN\\Documents\\GitHub\\Proyecto_Zeniaq\\NextP\\pages\\Contabilidad_electronica.js",
+    _this = undefined;
+
+
+
+var Contabilidad_electronica = function Contabilidad_electronica() {
+  return /*#__PURE__*/Object(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__["jsxDEV"])(_components_Container__WEBPACK_IMPORTED_MODULE_1__["default"], {}, void 0, false, {
+    fileName: _jsxFileName,
+    lineNumber: 6,
+    columnNumber: 5
+  }, _this);
+};
+
+_c = Contabilidad_electronica;
+/* harmony default export */ __webpack_exports__["default"] = (Contabilidad_electronica);
+
+var _c;
+
+$RefreshReg$(_c, "Contabilidad_electronica");
+
+;
+    var _a, _b;
+    // Legacy CSS implementations will `eval` browser code in a Node.js context
+    // to extract CSS. For backwards compatibility, we need to check we're in a
+    // browser context before continuing.
+    if (typeof self !== 'undefined' &&
+        // AMP / No-JS mode does not inject these helpers:
+        '$RefreshHelpers$' in self) {
+        var currentExports = module.__proto__.exports;
+        var prevExports = (_b = (_a = module.hot.data) === null || _a === void 0 ? void 0 : _a.prevExports) !== null && _b !== void 0 ? _b : null;
+        // This cannot happen in MainTemplate because the exports mismatch between
+        // templating and execution.
+        self.$RefreshHelpers$.registerExportsForReactRefresh(currentExports, module.i);
+        // A module can be accepted automatically based on its exports, e.g. when
+        // it is a Refresh Boundary.
+        if (self.$RefreshHelpers$.isReactRefreshBoundary(currentExports)) {
+            // Save the previous exports on update so we can compare the boundary
+            // signatures.
+            module.hot.dispose(function (data) {
+                data.prevExports = currentExports;
+            });
+            // Unconditionally accept an update to this module, we'll check if it's
+            // still a Refresh Boundary later.
+            module.hot.accept();
+            // This field is set when the previous version of this module was a
+            // Refresh Boundary, letting us know we need to check for invalidation or
+            // enqueue an update.
+            if (prevExports !== null) {
+                // A boundary can become ineligible if its exports are incompatible
+                // with the previous exports.
+                //
+                // For example, if you add/remove/change exports, we'll want to
+                // re-execute the importing modules, and force those components to
+                // re-render. Similarly, if you convert a class component to a
+                // function, we want to invalidate the boundary.
+                if (self.$RefreshHelpers$.shouldInvalidateReactRefreshBoundary(prevExports, currentExports)) {
+                    module.hot.invalidate();
+                }
+                else {
+                    self.$RefreshHelpers$.scheduleUpdate();
+                }
+            }
+        }
+        else {
+            // Since we just executed the code for the module, it's possible that the
+            // new exports made it ineligible for being a boundary.
+            // We only care about the case when we were _previously_ a boundary,
+            // because we already accepted this update (accidental side effect).
+            var isNoLongerABoundary = prevExports !== null;
+            if (isNoLongerABoundary) {
+                module.hot.invalidate();
+            }
+        }
+    }
+
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./../node_modules/next/dist/compiled/webpack/harmony-module.js */ "./node_modules/next/dist/compiled/webpack/harmony-module.js")(module)))
+
+/***/ })
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcGFnZXMvQ29udGFiaWxpZGFkX2VsZWN0cm9uaWNhLmpzIl0sIm5hbWVzIjpbIkNvbnRhYmlsaWRhZF9lbGVjdHJvbmljYSJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7Ozs7Ozs7OztBQUNBOztBQUVBLElBQU1BLHdCQUF3QixHQUFFLFNBQTFCQSx3QkFBMEI7QUFBQSxzQkFFNUIscUVBQUMsNkRBQUQ7QUFBQTtBQUFBO0FBQUE7QUFBQSxXQUY0QjtBQUFBLENBQWhDOztLQUFNQSx3QjtBQVFTQSx1RkFBZiIsImZpbGUiOiJzdGF0aWMvd2VicGFjay9wYWdlcy9Db250YWJpbGlkYWRfZWxlY3Ryb25pY2EuN2ZlZGVkNTQxZDNiMDJjZDNiZDAuaG90LXVwZGF0ZS5qcyIsInNvdXJjZXNDb250ZW50IjpbIlxyXG5pbXBvcnQgQ29udGFpbmVyIGZyb20gJy4uL2NvbXBvbmVudHMvQ29udGFpbmVyJ1xyXG5cclxuY29uc3QgQ29udGFiaWxpZGFkX2VsZWN0cm9uaWNhID0oKSA9PiAoXHJcbiAgICAgICAgXHJcbiAgICA8Q29udGFpbmVyPlxyXG4gICAgICAgIFxyXG48L0NvbnRhaW5lcj5cclxuKVxyXG5cclxuXHJcbmV4cG9ydCBkZWZhdWx0IENvbnRhYmlsaWRhZF9lbGVjdHJvbmljYTsiXSwic291cmNlUm9vdCI6IiJ9
