835ec7ef5b2e55952f88354817f8e94c9b6fc8fc
axbren <natsuevans1999@gmail.com>
2021-03-02 16:15:25 -0600

    atualizacion contabilidad electronica

--- NextP/Pages/Contabilidad_electronica.js
+++ NextP/Pages/Contabilidad_electronica.js
@@ -1,7 +1,124 @@
 import React from 'react'
+import Container from '../components/Container'
 
-const Contabilidad_electronica = () =>{
-    return <h1>Estas en contabilidad electronica</h1>
-}
+const Contabilidad_electronica =() => (
+        
+    <Container>
+        <Head>
+            <title>Contadores y Auditores Lopez Ocadiz</title>
+        </Head>
+        <center>
+        <h1>Integramos servicios y herramientas contables.</h1>
+        <p>Somos una empresa integrada por contadores públicos certificados, con una amplia experiencia profesional en materia de contaduría pública, asesoría fiscal y administrativa, </p>
+        </center>
+        <br></br>
+        <br></br>
+        <br></br>
+        <Image
+        borderRadius="full"
+boxSize="90px"
+src="https://thumbs.dreamstime.com/b/icono-computacional-de-la-nube-con-un-s%C3%ADmbolo-del-documento-vector-146856637.jpg"
+alt="Segun Adebayo"
+/>
+<Text fontSize="xs" color="gray.500" >CONTABILIDAD ELECTRÓNICA</Text>
+        <Button colorScheme="blue" size="xs">
+         <Link href="/Contabilidad_electronica">Leer mas</Link>
+        </Button>
+        <Image
+        borderRadius="full"
+boxSize="90px"
+src="https://images.vexels.com/media/users/3/129767/isolated/preview/547ed01097a7aaf9df985178b234a906-icono-de-lista-de-control-by-vexels.png"
+
+alt="Segun Adebayo"
+/>
+<Text fontSize="xs" color="gray.500">PRESENTACIÓN Y CUMPLIMIENTO DE OBLIGACIONES FISCALES
+</Text>
+        <Button colorScheme="blue" size="xs">
+            Leer mas
+        </Button>
+       
+<Image
+        borderRadius="full"
+boxSize="90px"
+src="https://image.flaticon.com/icons/svg/34/34733.svg"
+alt="Segun Adebayo"
+/>
+<Text fontSize="xs" color="gray.500">AUDITORIA DE ESTADOS FINANCIEROS</Text>
+        <Button colorScheme="blue" size="xs">
+            Leer mas
+        </Button>
+<br></br>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="90px"
+src="https://image.flaticon.com/icons/png/512/64/64302.png"
+alt="Segun Adebayo"/>
+        <Button colorScheme="blue" size="xs">
+            Button_4
+        </Button>
+<Text fontSize="xs" color="gray.500">ASESORÍA ADMINISTRATIVA Y CONTROL INTERNO</Text>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="90px"
+src="https://static.vecteezy.com/system/resources/previews/000/652/743/original/report-table-icon-vector.jpg"
+alt="Segun Adebayo"/>
+        <Button colorScheme="blue" size="xs">
+            Button_5
+        </Button>
+        <Text fontSize="xs" color="gray.500">ADMINISTRACIÓN DE DOCUMENTOS DIGITALES</Text>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="90px"
+src="https://images.vexels.com/media/users/3/131163/isolated/preview/751175c268ea1f69854ff67fb93ee1a2-el-juez-icono-del-c-rculo-de-martillo-by-vexels.png"
+alt="Segun Adebayo"/>
+        <Button colorScheme="blue" size="xs">
+            Button_6
+        </Button>
+        <Text fontSize="xs" color="gray.500">ORIENTACIÓN LEGAL Y FINANCIERA</Text>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="90px"
+src="https://th.bing.com/th/id/OIP.loHqV6lefcI_wbLt9IniewHaHa?pid=ImgDet&rs=1"
+alt="Segun Adebayo"/>
+        <Button colorScheme="blue" size="xs">
+            Button_7
+        </Button>
+        <Text fontSize="xs" color="gray.500">PLANEACIÓN CONTABLE Y FISCAL</Text>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="90px"
+src="https://th.bing.com/th/id/OIP.VaAarVYZywKOd4_68sGXdQHaHa?pid=ImgDet&rs=1"
+alt="Segun Adebayo"/>
+        <Button colorScheme="blue" size="xs">
+            Button_8
+        </Button>
+        <Text fontSize="xs" color="gray.500">DEVOLUCIONES Y COMPENSACIONES</Text>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="90px"
+src="https://i0.pngocean.com/files/695/297/280/bar-chart-computer-icons-statistics-graph-of-a-function-ascending-line.jpg"
+alt="Segun Adebayo"/>
+        <Button colorScheme="blue" size="xs">
+            Button_9
+        </Button>
+        <Text fontSize="xs" color="gray.500">PROYECTOS DE INVERSIÓN</Text>
+<br></br>
+<br></br>
+<br></br>
+<Image
+borderRadius="full"
+boxSize="150px"
+src="https://i0.pngocean.com/files/695/297/280/bar-chart-computer-icons-statistics-graph-of-a-function-ascending-line.jpg"
+alt="Segun Adebayo"/>
+
+</Container>
+)
+export default Services;
 
 export default Contabilidad_electronica;
