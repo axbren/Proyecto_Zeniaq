85874c005cad67ff6212d17ae2bcf805fc9b8cf9
axbren <natsuevans1999@gmail.com>
2021-03-03 16:46:45 -0600

    Cambios

--- NextP/Pages/index.js
+++ NextP/Pages/index.js
@@ -3,8 +3,7 @@ import Container from "../components/Container";
 import Users from "../components/Users";
 import {Image, ImageGroup} from "@chakra-ui/react"
 import { Input } from "@chakra-ui/react"
-
-
+import { Box } from "@chakra-ui/react"
 
 const Index = (props) => {
     console.log(props);
@@ -20,15 +19,29 @@ const Index = (props) => {
             <br></br>
             <br></br>
             <br></br>
+            <center>
+  <Box w="50%" p={4} color="white" bg="gray">
+      Nuestra Misión
+  </Box>
+  </center>
             <Image
-    boxSize="300px"
+    boxSize="200px"
     objectFit="cover"
     src="https://www.lopezocadizcontadores.com/wp-content/uploads/2018/10/cropped-achievement-agreement-arms-1068523-1.jpg"
     alt="Dan Abramov"
   />
+  
+   &nbsp;&nbsp;&nbsp;&nbsp; 
+   &nbsp;&nbsp;&nbsp;&nbsp; 
+   &nbsp;&nbsp;&nbsp;&nbsp; 
+   &nbsp;&nbsp;&nbsp;&nbsp; 
+   <center>
+  <Box w="50%" p={4} color="white" bg="gray">
+      Visión
+  </Box>
+  </center>
     <Image
-    align ="center"
-    boxSize="300px"
+    boxSize="200px"
     objectFit="cover"
     src="https://www.lopezocadizcontadores.com/wp-content/uploads/2018/10/cropped-mission.jpg"
     alt="Dan Abramov"
